1415a6028378c41a765d0a68a3d6193d50779c16
NabdipDEV <nabdipdevsharma@gmail.com>
2026-06-02 22:28:59 +0530

    improve UI with booking cards, dropdown, and animations

--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -3,18 +3,83 @@
 import { authClient } from "@/lib/auth-client";
 import { motion } from "framer-motion";
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 import { FaMoneyBillWave } from "react-icons/fa";
 
 const BookingCard = ({ facility }) => {
+    const [loading, setLoading] = useState(false);
     const { data: session, isPending } = authClient.useSession();
     const user = session?.user;
-    
+
     const [bookingDate, setBookinDate] = useState("")
-   
+    // const [price, _id,] = facility
+
+    const handelBooking = async () => {
+        if (!session?.user) {
+            toast.error("Please login first");
+            return;
+        }
+
+        if (!bookingDate) {
+            toast.error("Please select a booking date");
+            return;
+        }
+
+        if (!facility?._id) {
+            toast.error("Facility not found");
+            return;
+        }
+
+        setLoading(true);
+
+        const user = session.user;
+
+        const dataBooking = {
+            userId: user?.id || user?._id,
+            userName: user?.name || user?.email,
+            userEmail: user?.email,
+            userImage: user?.image || "",
+
+            facilityId: facility?._id,
+            facilityName: facility?.name,
+            facilitySport: facility?.sport,
+            facilityLocation: facility?.location,
+            facilityImage: facility?.image,
+
+            price: Number(facility?.price),
+            bookingDate,
+        };
+
+        try {
+            const res = await fetch("http://localhost:5000/booking", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(dataBooking),
+            });
+
+            const result = await res.json();
+
+            if (!res.ok) {
+                throw new Error(result?.message || "Booking failed");
+            }
+
+            toast.success("Booking successful!");
+            console.log(result);
+
+        } catch (error) {
+            console.error(error);
+            toast.error(error.message || "Something went wrong");
+
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div className="flex flex-col gap-4 bg-gradient-to-br from-emerald-400/20 via-green-300/20 to-lime-500/20 border border-slate-200 rounded-xl p-4">
-            <div className="grid grid-cols-6 gap-4 justify-between items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-between items-center">
 
                 <motion.div
                     whileHover={{ y: -3 }}
@@ -23,7 +88,7 @@ const BookingCard = ({ facility }) => {
                         stiffness: 300,
                         damping: 20,
                     }}
-                    className="flex col-span-3 items-center justify-between border border-blue-300 rounded-xl p-4"
+                    className="flex col-span-1 items-center justify-between border border-blue-300 rounded-xl p-4"
                 >
                     <div>
                         <p className="text-[11px] text-gray-500">Price per session</p>
@@ -42,7 +107,7 @@ const BookingCard = ({ facility }) => {
                         stiffness: 300,
                         damping: 20,
                     }}
-                    className="col-span-3 border border-blue-300 rounded-xl p-2"
+                    className="col-span-1 border border-blue-300 rounded-xl p-2"
                 >
                     <p className="text-[11px] text-gray-500 mb-1">
                         Booking Date :
@@ -57,6 +122,7 @@ const BookingCard = ({ facility }) => {
             </div>
 
             <motion.button
+                onClick={handelBooking}
                 whileHover={{ y: -2 }}
                 whileTap={{ scale: 0.98 }}
                 transition={{
@@ -64,7 +130,7 @@ const BookingCard = ({ facility }) => {
                     stiffness: 400,
                     damping: 25,
                 }}
-                className="w-full py-3 rounded-xl bg-gradient-to-r from-blue-600 to-blue-700 text-white text-sm font-semibold relative overflow-hidden group"
+                className="w-full py-3 cursor-pointer rounded-xl bg-gradient-to-r from-blue-600 to-blue-700 text-white text-sm font-semibold relative overflow-hidden group"
             >
                 <span className="absolute inset-0 bg-white/20 translate-x-[-100%] group-hover:translate-x-[100%] transition duration-700" />
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,15 @@ import { IoIosLogOut } from "react-icons/io";
 import { RiHome4Line } from "react-icons/ri";
 import { MdOutlineOpenInBrowser, MdWavingHand } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import { IoCheckmarkDone } from "react-icons/io5";
+import { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
   const { data: session, isPending } = authClient.useSession();
   const user = session?.user;
+
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
   const pathname = usePathname();
 
@@ -20,6 +25,19 @@ const Navbar = () => {
     window.location.reload();
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="navbar bg-white shadow-md px-6 sticky top-0 z-50">
@@ -61,14 +79,6 @@ const Navbar = () => {
           <li><NavLink href="/my-bookings">My Bookings</NavLink></li>
           <li><NavLink href="/add-facility">Add Facility</NavLink></li>
           <li><NavLink href="/manage-facilities">Manage Facilities</NavLink></li>
-          {user && (
-            <li>
-              <NavLink href="/my-profile">
-                <CgProfile />
-                My Profile
-              </NavLink>
-            </li>
-          )}
         </ul>
       </div>
 
@@ -83,10 +93,52 @@ const Navbar = () => {
             <img
               src={user?.image || "https://i.ibb.co/4pDNDk1/avatar.png"}
               alt="user"
-              className="z-10 w-7 h-7 rounded-full -mr-10 object-cover"
+              className="z-10 w-6 h-6 rounded-full -mr-9 object-cover"
             />
 
-            <button
+            <div ref={dropdownRef} className="relative inline-block">
+
+              {/* BUTTON (select look) */}
+              <button
+                onClick={() => setOpen(!open)}
+                className="flex items-center justify-center gap-2 font-semibold relative pl-8 pr-3 py-1 text-[#000000fa]
+    hover:text-white text-[16px] border border-[#0e0500]
+    hover:bg-gradient-to-r from-[#4300fb] to-[#1a3cffe7]
+    rounded-full cursor-pointer transition"
+              >
+                Profile
+               <span className="text-[13px]">▼</span>
+              </button>
+
+              {/* DROPDOWN */}
+              {open && (
+                <div className="absolute p-2 right-0 mt-2 w-40 bg-white border rounded-xl shadow-lg z-50">
+
+                  <Link
+                    href="/my-profile"
+                    className="w-full flex items-center gap-2 px-4 py-2 rounded-2xl font-semibold text-black hover:text-blue-500 hover:bg-blue-100"
+                    onClick={() => setOpen(false)}
+                  >
+                    <CgProfile />
+                    My Profile
+                  </Link>
+
+                  <button
+                    className="w-full flex items-center gap-2 px-4 py-2 rounded-2xl hover:bg-red-100 text-red-600"
+                    onClick={() => {
+                      handleLogout();
+                      setOpen(false);
+                    }}
+                  >
+                    <IoIosLogOut />
+                    Logout
+                  </button>
+
+                </div>
+              )}
+            </div>
+
+            {/* <button
               onClick={handleLogout}
               className="flex items-center gap-1 font-semibold relative pl-9 pr-4 py-1 text-[#000000fa]
         hover:text-white text-lg border border-[#0e0500]
@@ -95,7 +147,7 @@ const Navbar = () => {
             >
               <IoIosLogOut />
               logout
-            </button>
+            </button> */}
 
           </>
         ) : (
